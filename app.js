d8c0a39d340fcb4f492d9ffc54fc45b9f8bb78c0
abakharev <abakharev@sharevault.com>
2022-04-26 18:17:37 +0300

    replace hbs with react

--- app.js
+++ app.js
@@ -6,28 +6,29 @@ const PORT = 3000;
 
 // Тут должна быть проверка подключения к БД.
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   // Отображает список коротких URL
-  res.render('index', { title: 'Url shortener' });
+  res.render('Main', { title: 'Url shortener' });
 });
 
-app.post('/urls', function (req, res) {
+app.post('/urls', (req, res) => {
   // Создать новый объект 'Url'
   // Автоматически создаются короткие  URL
   // В конце надо вернуться обратно на домашнюю страницу
 });
 
-app.get('/:shortUrl', function (req, res, next) {
+app.get('/:shortUrl', (req, res, next) => {
   // Перейти по короткому к соответствующему "длинному" URL
 });
 
 app.listen(PORT, () => {
   console.log(`server started PORT: ${PORT}`);
-})
+});
