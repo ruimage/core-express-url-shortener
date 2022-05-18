11b65aee5d9421145360b97707d3ce65946d650b
abakharev <abakharev@sharevault.com>
2022-05-18 11:54:25 +0300

    replace hbs with react

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -4,7 +4,7 @@ module.exports = function Layout({ children, title }) {
   return (
     <html lang="en">
       <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta
           name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
